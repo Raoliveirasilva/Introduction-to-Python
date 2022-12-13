627eefc6d6a7f65bcd70d0332431c75ba03cc984
Ramon Oliveira Silva <105325488+Raoliveirasilva@users.noreply.github.com>
2022-12-13 18:42:35 -0300

    DESAFIO
    Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.

---
+++ Desafio_2.py
@@ -0,0 +1,40 @@
+mes = int(input("Informe um numero positivo de 1 a 12: "))
+
+
+months = "january"
+months2 = "february"
+months3 = "march"
+months4 = "april"
+months5 = "may"
+months6 = "june"
+months7 = "july"
+months8 = "august"
+months9 = "september"
+months10 = "october"
+months11 = "november"
+months12 = "december"
+
+if mes == 1:
+    print(months.title())
+elif mes == 2:
+    print(months2.title())
+elif mes == 3:
+    print(months3.title())
+elif mes == 4:
+    print(months4.title())
+elif mes == 5:
+    print(months5.title())
+elif mes == 6:
+    print(months6.title())
+elif mes == 7:
+    print(months7.title())
+elif mes == 8:
+    print(months8.title())
+elif mes == 9:
+    print(months9.title())
+elif mes == 10:
+    print(months10.title())
+elif mes == 11:
+    print(months11.title())
+elif mes == 12:
+    print(months12.title())
