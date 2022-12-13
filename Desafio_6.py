ab15f39956d4a77c825d2b8daf641cad65a36690
Ramon Oliveira Silva <105325488+Raoliveirasilva@users.noreply.github.com>
2022-12-13 17:10:02 -0300

    DESAFIO
    Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.

---
+++ Desafio_6.py
@@ -0,0 +1,27 @@
+a = input() 
+b = input() 
+c = input() 
+
+if a == 'vertebrado' and b == "mamifero" and c == "onivoro":
+  print("homem")
+  
+elif a == 'invertebrado' and b == "inseto" and c == "hematofago":
+  print("pulga")
+  
+elif a == "vertebrado" and b == "mamifero" and c == "herbivoro":
+  print("vaca")
+
+elif a == "vertebrado" and b == "ave" and c == "carnivoro":
+  print("aguia")
+
+elif a == "vertebrado" and b == "ave" and c == "onivoro":
+  print("pomba")
+  
+elif a == "invertebrado" and b == "inseto" and c == "herbivoro":
+  print("largata")
+
+elif a == "invertebrado" and b == "anelideo" and c == "hematofago":
+  print("sanguessuga")
+
+elif a == "invertebrado" and b == "anelideo" and c == "onivoro":
+  print("minhoca")  
